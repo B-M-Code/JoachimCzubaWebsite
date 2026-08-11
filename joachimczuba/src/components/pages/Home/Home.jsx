f9d872395ec518ce8dcf3907e999970207a27fea
maks <nazwiskomaks29@gmail.com>
2026-08-12 01:25:46 +0200

    Implement mouse movement effects on Home page background

--- joachimczuba/src/components/pages/Home/Home.jsx
+++ joachimczuba/src/components/pages/Home/Home.jsx
@@ -1,11 +1,31 @@
+import { useRef, useCallback } from "react";
 import { Link } from "react-router-dom";
 
 import H from "./Home.module.scss";
 
 export default function Home() {
+  const bgRef = useRef(null);
+  const frameRef = useRef(null);
+
+  const handleMouseMove = useCallback((e) => {
+    if (!bgRef.current) return;
+
+    const x = (e.clientX / window.innerWidth) * 2 - 1;
+    const y = (e.clientY / window.innerHeight) * 2 - 1;
+
+    bgRef.current.style.setProperty("--x", x.toFixed(3));
+    bgRef.current.style.setProperty("--y", y.toFixed(3));
+  }, []);
+
+  const handleMouseLeave = useCallback(() => {
+    if (!bgRef.current) return;
+    bgRef.current.style.setProperty("--x", 0);
+    bgRef.current.style.setProperty("--y", 0);
+  }, []);
+
   return (
-    <main>
-      <div className={H.frame}>
+    <main onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
+      <div className={H.frame} ref={frameRef}>
         <div></div>
         <div className={H.bottom__content}>
           <div className={H.wrapper}>
@@ -26,7 +46,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className={H.bg}></div>
+      <div className={H.bg} ref={bgRef}></div>
     </main>
   );
 }
