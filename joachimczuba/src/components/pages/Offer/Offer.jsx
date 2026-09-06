b47db99b701ff6239783fa63e3115ac23343ba66
maks <nazwiskomaks29@gmail.com>
2026-09-06 13:29:33 +0200

    offercard

--- joachimczuba/src/components/pages/Offer/Offer.jsx
+++ joachimczuba/src/components/pages/Offer/Offer.jsx
@@ -11,9 +11,38 @@ export default function Offer() {
           <div className={styles.img}></div>
         </div>
       </main>
-
-      <div className={styles.OfferCards_container}>
-        <section></section>
+      <SubPageHeaderBlock
+        title={"Oferta"}
+        text={
+          "Wybierz plan dopasowany do swoich celów. Skuteczna kalistenika oparta na wiedzy i doświadczeniu."
+        }
+        subtext={"Im szybciej zaczniesz, tym szybciej zobaczysz efekty."}
+      />
+      <div className={styles.offercards_container}>
+        <section className={styles.offercard}>
+          <div className={styles.offercard_wrapper}>
+            <div className={styles.text_container}>
+              <div className={styles.header}>
+                <div className={styles.line}></div>
+                <h2>start progresji</h2>
+                <p>
+                  Optymalny plan treningowy zapewniający prawidłową progresję i
+                  samodzielne budowanie formy.
+                </p>
+              </div>
+              <div className={styles.line}></div>
+              <ol>
+                <li>video instruktażowe</li>
+                <li>plan treningowy</li>
+              </ol>
+            </div>
+            <div className={styles.img}></div>
+          </div>
+          <div className={styles.price_wrapper}>
+            <p>99 zł</p>
+            <button>Wybierz</button>
+          </div>
+        </section>
       </div>
     </>
   );
